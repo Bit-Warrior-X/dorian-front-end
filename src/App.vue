0f7de140b256ed9b8e0fd3e76aa5e1efa3bc79f5
bitwarriorx <bitwarrior00007@gmail.com>
2026-01-13 13:49:44 +0200

    update missing file

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import LoginPage from './components/LoginPage.vue'
+import MainPage from './components/MainPage.vue'
+
+const isLoggedIn = ref(false)
+
+const handleLoginSuccess = () => {
+  isLoggedIn.value = true
+}
+
+const handleLogout = () => {
+  isLoggedIn.value = false
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <LoginPage v-if="!isLoggedIn" @login-success="handleLoginSuccess" />
+  <MainPage v-else @logout="handleLogout" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
 }
 </style>
